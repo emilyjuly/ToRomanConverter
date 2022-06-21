51849a54cb83b5455acb8284b9911c520908fea8
emilyjuly <emilyjulygd@gmail.com>
2022-06-21 19:19:33 -0300

    Upando apenas a função em JS funcionado, ainda sem interação com o HTML

---
+++ script.js
@@ -0,0 +1,30 @@
+function convertToRoman (num){
+    const guide = {
+        1: 'I',
+        4: 'IV',
+        5: 'V',
+        9: 'IX',
+        10: 'X',
+        40: 'XL',
+        50: 'L',
+        90: 'XC',
+        100: 'C',
+        400: 'CD',
+        500: 'D',
+        900: 'CM',
+        1000: 'M',
+    };
+
+    let roman = '';
+    const reverseGuide = Object.keys(guide).reverse();
+
+    reverseGuide.forEach(key => {
+        while (key <= num){
+            roman += guide[key];
+            num -= key;
+        }
+    });
+    return roman;
+}
+
+console.log (convertToRoman(2));
